6a32eb183b8db9231c4e8ad86ecdb527d4e3aa80
Blunt0FF <taekwondo2701@gmail.com>
2025-06-09 17:28:18 +0200

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,18 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4000
+    port: 4000,
+    strictPort: true,
+    host: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
   }
 })
