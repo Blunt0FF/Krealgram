44cabadcc9811c5a7d7219023e5beda94d22994f
Kreal Blunt0ff <admin@Kreals-MacBook-Pro.local>
2025-06-13 14:50:22 +0200

    Updated backend

--- backend/routes/followRoutes.js
+++ backend/routes/followRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/authMiddleware');
-const User = require('../models/User');
+const User = require('../models/userModel');
 
 // Подписаться на пользователя
 router.post('/:userId', auth, async (req, res) => {
